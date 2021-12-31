c83ce3c639fc5795688b3672e2652f6ddab0b57b
obah2s <bahsylla96@gmail.com>
2021-12-31 10:08:56 +0100

    Add login Form

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  hide = true;
+  loginForm = new FormGroup({
+    email : new FormControl('', [Validators.required, Validators.email]),
+    password : new FormControl(''),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value);
+  }
 }
